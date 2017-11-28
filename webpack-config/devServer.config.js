aa30a1dc97c641f2c0dd4c073f8176307e43efe5
Pama <625706468@qq.com>
2017-11-28 10:59:35 +0800

    devServer proxy

--- webpack-config/devServer.config.js
+++ webpack-config/devServer.config.js
@@ -3,6 +3,13 @@ const devServerConfig = {
 	hot: true,
 	inline: true,
 	progress: true,
-	compress: true
+	compress: true,
+	proxy: {
+		'/interface/*': {
+			target: 'http://localhost:80',
+			changeOrigin: true,
+			secure: false
+		}
+	}
 };
 module.exports = devServerConfig;
