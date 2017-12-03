80f0196902c5e7bdd622ed2cf270474561cf1151
Tutu1993 <625706468@qq.com>
2017-12-03 16:02:00 +0800

    update legal/contact

--- src/html/legal/contact/index.js
+++ src/html/legal/contact/index.js
@@ -10,3 +10,33 @@ ReactDOM.render(
 	<CitySelector />,
 	document.querySelector('#city-selector')
 );
+
+function fetchDemo() {
+    const form = new FormData(document.querySelector('#form-contact'));
+	
+    fetch('/interface/post.php', {
+        method: 'POST',
+        body: form
+    })
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function(data) {
+                  console.log(data);
+            });
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+const submit = document.querySelector('#form-submit');
+
+submit.addEventListener('click', function(){
+	fetchDemo();
+});
