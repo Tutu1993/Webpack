759beba3068e2269f771f36151fc8c761780c658
Pama <625706468@qq.com>
2018-01-09 18:01:52 +0800

    首页部分

--- src/html/index/index.js
+++ src/html/index/index.js
@@ -2,11 +2,27 @@ require('cssDir/index/index.css')
 
 const log = console.log.bind(console)
 
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-banner', {
 	direction: 'vertical',
 	loop: true,
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.pagination-banner',
 		clickable: true,
 	},
 })
+
+const coverflow = new Swiper('.swiper-coverflow', {
+	effect: 'coverflow',
+	slidesPerView: 5,
+	loop: true,
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	coverflowEffect: {
+		rotate: 60,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows : true,
+	},
+});
