88cc3cac3edf05bbd58ad196b4a5f039c1d1a0f6
Pama <625706468@qq.com>
2018-01-11 15:59:24 +0800

    暂存 第二个页面

--- src/html/recruit/index.js
+++ src/html/recruit/index.js
@@ -1,1 +1,31 @@
 require('cssDir/recruit/index.css')
+
+function fetchDemo() {
+    const form = new FormData(document.querySelector('#form-contact'))
+
+    fetch('/interface/post.php', {
+        method: 'POST',
+        body: form,
+    })
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status)
+                return
+            }
+
+            // Examine the text in the response
+            response.json().then(function(data) {
+				console.log(data)
+            })
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err)
+        })
+}
+
+const submit = document.querySelector('#form-submit')
+
+submit.addEventListener('click', function(){
+	fetchDemo()
+})
