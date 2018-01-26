b7a9af3c7da5f0f92d3d3e5e20a9d4636dec3a59
Pama <625706468@qq.com>
2018-01-26 11:54:36 +0800

    修改

--- src/html/index/index.js
+++ src/html/index/index.js
@@ -3,6 +3,10 @@ require('cssDir/index/index.css')
 const log = console.log.bind(console)
 
 const swiper = new Swiper('.swiper-banner', {
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
 	direction: 'vertical',
 	loop: true,
 	pagination: {
@@ -11,9 +15,11 @@ const swiper = new Swiper('.swiper-banner', {
 	},
 })
 
+const listArray = ["", "list-1", "list-2", "list-3", "list-4"]
+const shopArray = ["", "江苏省昆山市周庄景区店", "北京市定福庄店", "上海市三林地铁站店", "江苏省昆山市昆山大戏院店"]
 const coverflow = new Swiper('.swiper-coverflow', {
 	effect: 'coverflow',
-	slidesPerView: 5,
+	slidesPerView: 4,
 	loop: true,
 	grabCursor: true,
 	centeredSlides: true,
@@ -24,5 +30,20 @@ const coverflow = new Swiper('.swiper-coverflow', {
 		depth: 100,
 		modifier: 1,
 		slideShadows : true,
+	},
+	// autoplay: {
+	// 	delay: 5000,
+	// 	disableOnInteraction: false,
+	// },
+	on: {
+		transitionEnd: function(){
+			const id = document.querySelector(".swiper-coverflow .swiper-slide-active").children[0].classList[0]
+
+			document.querySelector("#shop-id").innerHTML = shopArray[listArray.indexOf(id)]
+		},
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
 	},
 });
