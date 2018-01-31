9d81fdfa88d2caa0e84cc41412b5ad276953ca50
Tutu1993 <625706468@qq.com>
2018-01-31 20:15:54 +0800

    嘛 就先这样了

--- src/html/recruit/index.js
+++ src/html/recruit/index.js
@@ -18,25 +18,37 @@ const swiper = new Swiper('.swiper-banner', {
 function fetchDemo() {
     const form = new FormData(document.querySelector('#form-contact'))
 
-    fetch('http://zero.puziapp.com.cn/investment/add', {
-        method: 'POST',
-        body: form,
-    })
-        .then(
-            function(response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' + response.status)
-                return
-            }
+	if (!/^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/.test(form.get('name'))) {
+		alert('请确认为姓名为中文')
+		return
+	}
+	if (form.get('phone') === '') {
+		alert('请确认手机号不为空')
+		return
+	}
+	fetch('http://zero.puziapp.com.cn/investment/add', {
+		method: 'POST',
+		body: form,
+	})
+		.then(
+			function(response) {
+				if (response.status !== 200) {
+					console.log('Looks like there was a problem. Status Code: ' + response.status)
+				return
+			}
 
-            // Examine the text in the response
-            response.json().then(function(data) {
-				console.log(JSON.stringify(data))
-            })
-        })
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err)
-        })
+			// Examine the text in the response
+			response.json().then(function(data) {
+				if (data.code === 0) {
+					alert('信息提交成功')
+				} else {
+					alert('信息提交失败，请联系管理员')
+				}
+			})
+		})
+		.catch(function(err) {
+			console.log('Fetch Error :-S', err)
+		})
 }
 
 const submit = document.querySelector('#form-submit')
